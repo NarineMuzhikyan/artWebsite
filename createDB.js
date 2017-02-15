569bb651e83ceb2e64b41409cb9060e0fd3d5004
NarineMuzhikyan <nmuzhikyan@gmail.com>
2017-02-15 18:59:23 +0400

    make server part

--- createDB.js
+++
@@ -1,9 +0,0 @@
-// Retrieve
-const MongoClient = require('mongodb').MongoClient;
-
-// Connect to the db
-MongoClient.connect("mongodb://localhost:1337", function(err, db) {
-    if(!err) {
-        console.log("We are connected");
-    }
-});
